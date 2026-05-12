062d8852a0e527e912d57ca69e8560051e99ab47
AdityaIyan <adityafieansyah@gmail.com>
2026-05-13 01:06:26 +0700

    make versel json

---
+++ api/index.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors({ origin: true, credentials: true });
+  await app.init();
+}
+
+let bootstrapped = false;
+
+export default async function handler(req: any, res: any) {
+  if (!bootstrapped) {
+    await bootstrap();
+    bootstrapped = true;
+  }
+  server(req, res);
+}
